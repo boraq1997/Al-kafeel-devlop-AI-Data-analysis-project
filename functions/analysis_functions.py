bb3ccf12af895cea2c49c1900a51a0256e3ca2ff
unknown <boraqnz@gmail.com>
2024-07-30 19:25:00 +0300

    2024-7-30

--- functions/analysis_functions.py
+++ functions/analysis_functions.py
@@ -8,8 +8,30 @@ def CSVFileReading(path):
     return dataFrame
 
 def dataAnalysisPage():
-    analysisPage = imf.Tk()
+    analysisPage = imf.Toplevel()
     analysisPage.title("Analysis Page")
     analysisPage.geometry("900x700")
 
-    
+    analysisPage.rowconfigure(0, weight=1)
+    analysisPage.rowconfigure(1, weight=1)
+    analysisPage.rowconfigure(2, weight=1)
+    analysisPage.rowconfigure(3, weight=1)
+
+    analysisPage.columnconfigure(0, weight=1)
+    analysisPage.columnconfigure(1, weight=1)
+    analysisPage.columnconfigure(2, weight=1)
+
+    img = "analysis.png"
+    def test():
+        img = "bar-chart.png"
+
+    mainFrame = imf.ctk.CTkFrame(analysisPage)
+    mainFrame.grid(row=0, column=0, columnspan=3, rowspan=4, sticky="news")
+
+    chartImage = imf.Image.open(f"files/images/CHARTS/{img}").resize((250, 250))
+    chartImageTk = imf.ImageTk.PhotoImage(chartImage)
+    chartImageLabel = imf.ctk.CTkLabel(mainFrame, image=chartImageTk)
+
+    chartImageLabel.pack(padx=0, pady=0)
+
+    button = imf.ctk.CTkButton(mainFrame, text="TEST", command=test).pack()
--- test.py
+++ test.py
@@ -1,55 +1,6 @@
-import importsFile as imf
-
-fileChoicedPath = ""
-
-def callSettingsFunction():
-    imf.func.settingsButton(root)
-
-root = imf.tk.Tk()
-root.title("data analysis")
-root.geometry("1000x700")
-root.option_add("*tearOff", False)
-root.resizable(False, False)
-
-style = imf.ttk.Style(root)
-root.tk.call("source", f"forest-dark.tcl")
-style.theme_use(f"forest-dark")
-
-icon_path = 'files/images/logo.ico'
-icon = imf.Image.open(icon_path)
-icon = imf.ImageTk.PhotoImage(icon)
-root.iconphoto(True, icon)
-
-root.columnconfigure(0, weight=1)
-root.columnconfigure(1, weight=1)
-root.columnconfigure(2, weight=1)
-root.columnconfigure(3, weight=1)
-root.columnconfigure(4, weight=1)
-
-root.rowconfigure(0, weight=1)
-root.rowconfigure(1, weight=1)
-root.rowconfigure(2, weight=1)
-root.rowconfigure(3, weight=1)
-root.rowconfigure(4, weight=1)
-
-sideBar = imf.ttk.Frame(root).pack()
-
-sideBarHomeIco = imf.Image.open("files/images/home.png").resize((30, 30))
-sideBarAboutusIco = imf.Image.open("files/images/information-button.png").resize((30, 30))
-sideBarSettingsIco = imf.Image.open("files/images/settings.png").resize((30, 30))
-sideBarAppInfoIco = imf.Image.open("files/images/curriculum-vitae.png").resize((30, 30))
-sideBarExitIco = imf.Image.open("files/images/logout.png").resize((30, 30))
-
-sideBarHomeIco = imf.ImageTk.PhotoImage(sideBarHomeIco)
-sideBarAboutusIco = imf.ImageTk.PhotoImage(sideBarAboutusIco)
-sideBarSettingsIco = imf.ImageTk.PhotoImage(sideBarSettingsIco)
-sideBarAppInfoIco = imf.ImageTk.PhotoImage(sideBarAppInfoIco)
-sideBarExitIco = imf.ImageTk.PhotoImage(sideBarExitIco)
-
-sideBarHomeBtn = imf.ttk.Button(sideBar, text="HOME", image=sideBarHomeIco, compound=imf.LEFT).pack(fill="x", pady=5)
-sideBarAboutusBtn = imf.ttk.Button(sideBar, text="ABOUT US", image=sideBarAboutusIco, compound=imf.LEFT).pack(fill="x", pady=5)
-sideBarSettingsBtn = imf.ttk.Button(sideBar, text="SETTINGS", image=sideBarSettingsIco, compound=imf.LEFT, command=callSettingsFunction).pack(fill="x", pady=5)
-sideBarAppInfoBtn = imf.ttk.Button(sideBar, text="APPLICATION INFO", image=sideBarAppInfoIco, compound=imf.LEFT, command=imf.func.applicationInfoButton).pack(fill="x", pady=5)
-sideBarExitBtn = imf.ttk.Button(sideBar, text="EXIT", image=sideBarExitIco, compound=imf.LEFT, command=imf.func.exitButton).pack(fill="x", pady=5, side="bottom")
-
-root.mainloop()
+BARS
+PIE
+LINE 
+#  SEABORN
+boxplots
+Horizontal boxplot with observations
